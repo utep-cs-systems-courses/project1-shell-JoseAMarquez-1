e35cfa00780fd24527e84f697ea0bbe692874148
JoseAMarquez <ja-marqquezz@hotmail.com>
2020-09-14 22:46:38 -0700

    Redirection, pipies amd task was introduced

--- shell/shell.py
+++ shell/shell.py
@@ -2,6 +2,29 @@
 
 import os, sys, time, re
 
+
+def newProcess(args):
+    pid = os.getpid()
+    rc = os.fork()
+
+    if rc < 0:
+        sys.exit(1)
+
+    elif rc == 0:
+        for dir in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dir, args[0])
+        try:
+            os.execve(program, args, os.environ)
+        except FileNotFoundError:
+            pass
+
+        os.write(2, ("Could not exec %s\n" % args[0]).encode())
+        sys.exit(1)
+
+    else:
+        childPidCode = os.wait()
+
+
 running_state = 1
 shell = '$'
 
@@ -10,20 +33,20 @@ while running_state:
     os.write(1, (shell).encode())
 
     userInput = input()
-    userInput = userInput.split(" ")
+    userInputList = userInput.split(" ")
 
     #if exit change running state to 0 (while loop terminated)
-    if userInput[0] == 'exit' :
-        os.write(1, ("The System cannot find the path specidied ").encode())
+    if userInputList[0] == 'exit' :
+        os.write(1, ("Exiting Shell...").encode())
         running_state = 0
         exit()
 
-    elif userInput[0] == 'cd':
-        if len(userInput) > 2:
+    elif userInputList[0] == 'cd':
+        if len(userInputList) > 2:
             os.write(1, ("The System cannot find the path specidied ").encode())
             pass
         else:
-            newDir = userInput[1]
+            newDir = userInputList[1]
             os.write(1,(newDir).encode())
         try:
             os.chdir(newDir)
@@ -32,3 +55,71 @@ while running_state:
             os.write(1, ("The System cannot find the path specidied \n").encode())
             pass
         continue
+
+
+
+    elif userInputList[1] == '>':
+        if len(userInputList) > 2:
+            os.write(1, ("The System cannot find the path specidied ").encode())
+            pass
+        else:
+            pid = os.getpid()
+            rc = os.fork()
+
+            if rc < 0:
+                os.write(2, ("fork failed, returning %d\n" % rc).encode())
+                sys.exit(1)
+            elif rc == 0:
+                args = [userInputList[0] ,userInputList[2]]
+                os.close(1)
+                os.open(userInputList[2], os.O_CREAT | os.WRONLY);
+                os.set_inheritable(1,True)
+
+                for dir in re.split(":", os.environ['PATH']):
+                    program = "%s/%s" % (dir, args[0])
+                    try:
+                        os.execve(program,args, os.environ)
+                    except FileNotFoundError:
+                        pass
+                sys.exit(1)
+            else:
+                childPidCode = os.wait()
+                os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
+                 childPidCode).encode())
+
+
+    elif userInputList[1] == '|':
+        if len(userInputList) > 2:
+            os.write(1, ("The System cannot find the path specidied ").encode())
+            pass
+        else:
+            pid  = os.getpid()
+            pr, pw = os.pipe()
+
+            for f in (pr,pw):
+                os.set_inheritable(f,True)
+
+            rc = os.fork()
+
+            if rc < 0:
+                print("fork failed, returning %d\n" % rc, file=sys.stderr)
+                pass
+
+            elif rc == 0:
+                args = [i.strip() for i in re.split('[\x7c]', userInput)]
+                os.close(1)
+                os.dup(pr,pw)
+                os.set_inheritable(1, True)
+
+                for fd in (pr,pw):
+                    os.close(fd)
+                newProcess(args[0].split())
+            else:
+                os.close(0)
+                os.dup(pr)
+                for fd in (pw, pr):
+                    os.close(fd)
+                newProcess(args[1].split())
+
+    else:
+        newProcess(userInputList)
